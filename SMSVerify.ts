35109a1e0cc9d6ab64a9c7f1dfc9bc103d88010b
zjdx1998 <zjdx1998@gmail.com>
2019-09-05 13:52:49 +0800

    first init

---
+++ SMSVerify.ts
@@ -0,0 +1,40 @@
+/*
+    @version: 1.0
+    @author: 71117123张建东
+    @date: 2019/9/4
+*/
+//npm install qcloudsms_js --save
+var sc = require('./secret.ts')
+var QCloudSMS = require("qcloudsms_js");
+var fs = require('fs');
+let jsondata = JSON.parse(fs.readFileSync('smsconfig.json'));
+console.log(jsondata);
+var appID = sc.Decrypt(jsondata.appID);
+var appKEY = sc.Decrypt(jsondata.appKEY);
+var templatedID = sc.Decrypt(jsondata.templatedID);
+var smsSign = jsondata.smsSign;
+var qcloudsms = QCloudSMS(appID, appKEY);
+var ssender = qcloudsms.SmsSingleSender;
+function genCode(){
+    var str = "";
+    for(var i=1;i<=6;i++)
+        str+=Math.floor(Math.random()*10);
+    return parseInt(str);
+}
+
+function callback(err, res, resData) {
+    if (err) {
+        console.log("err: ", err);
+    } else {
+        console.log("request data: ", res.req);
+        console.log("response data: ", resData);
+    }
+}
+
+function SMSVerify(phoneNumber){
+    let VerifyCode = genCode();
+    ssender(86, phoneNumber, templatedID, [VerifyCode], smsSign, "", "", callback);
+    return VerifyCode;
+}
+
+module.exports = SMSVerify;
---
+++ index.ts
@@ -0,0 +1,74 @@
+console.log("-------Welcome to foof-SMSServer!-------");
+const inquirer = require('inquirer');
+
+const promptList = [{
+    type: 'input',
+    message: 'Please input your appID',
+    name: 'appID',
+},{
+    type: 'input',
+    message: 'Please input your appKey',
+    name: 'appKEY',
+},{
+    type: 'input',
+    message: 'Please input your templateID',
+    name: 'templatedID'
+},{
+    type: 'input',
+    message: "Please input your SMSSign",
+    name: 'smsSign'
+}
+];
+
+function callback(err, data){
+    if(err){console.log(err)};
+    console.log('Success');
+}
+var fs = require('fs');
+var sc = require('./secret.ts');
+function getQCloudSMSInfo(){
+    let ans;
+    inquirer.prompt(promptList).then(
+        answers=>{
+            answers.appID = sc.Encrypt(answers.appID);
+            answers.appKEY = sc.Encrypt(answers.appKEY);
+            answers.templatedID = sc.Encrypt(answers.templatedID);
+            ans = answers;
+        }
+    ).then(
+        ()=>{
+            let str = JSON.stringify(ans,null,'\t');
+            fs.writeFile('./smsconfig.json',str, callback);
+        }
+    )
+    
+}
+
+const deletePromptList = {
+    type: 'confirm',
+    name: 'delete',
+    message: 'Do you want to clean the configuration?',
+    default: false
+}
+
+fs.exists('smsconfig.json',function(exists){
+    if(!exists) {
+        console.log("-------Please configure your QCloudSMS Info!-------");
+        getQCloudSMSInfo();
+    }
+    else{
+        inquirer.prompt(deletePromptList).then(
+            answers=>{
+                if (answers.delete===true){
+                    fs.unlink('smsconfig.json', function(error){
+                        if(error){
+                            console.log(error);
+                            return false;
+                        }
+                        console.log("Clean successfully, now you can reconfigure your settings!");
+                    });
+                }
+            }
+        )
+    }
+});
